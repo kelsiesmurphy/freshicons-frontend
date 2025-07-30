919e89aafc050518b565ff5056565db3abfbf868
Kelsie Murphy <kelsie@kelsiesmurphy.com>
2025-07-30 13:57:40 +0100

    Removal of api app

--- apps/api/modules/lib/supabase.ts
+++
@@ -1,7 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-import { environment } from "@zuplo/runtime";
-
-export const supabase = createClient(
-  environment.SUPABASE_URL!,
-  environment.SUPABASE_SERVICE_ROLE_KEY!
-);
